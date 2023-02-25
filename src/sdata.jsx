1ea67818edeae3d2e8ffafebb5e27dc990a11107
Towhid <towhid0845@gmail.com>
2023-02-25 09:11:18 +0600

    This is first commit

---
+++ src/sdata.jsx
@@ -0,0 +1,39 @@
+const sdata = [
+	{
+		sname: "DARK",
+		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+		title: "A Netflix Original Series",
+		links: "https://www.netflix.com/in/title/80990668?source=35",
+	},
+	{
+		sname: "dfgdgf",
+		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+		title: "A Netflix Original Series",
+		links: "https://www.netflix.com/in/title/80990668?source=35",
+	},
+	{
+		sname: "erter",
+		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+		title: "A Netflix Original Series",
+		links: "https://www.netflix.com/in/title/80990668?source=35",
+	},
+	{
+		sname: "DAuutyuRK",
+		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+		title: "A Netflix Original Series",
+		links: "https://www.netflix.com/in/title/80990668?source=35",
+	},
+	{
+		sname: "mgghjh",
+		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+		title: "A Netflix Original Series",
+		links: "https://www.netflix.com/in/title/80990668?source=35",
+	},
+	{
+		name: "kyktre",
+		imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+		title: "A Netflix Original Series",
+		links: "https://www.netflix.com/in/title/80990668?source=35",
+	},
+];
+export default sdata;
